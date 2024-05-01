9f1f38bb9fa92d2647bcf3015d5d07036f896201
Sulayman334 <148399915+Sulayman334@users.noreply.github.com>
2024-05-01 01:15:54 +0000

    another commit

---
+++ src/StockCommission.java
@@ -0,0 +1,16 @@
+public class StockCommission {
+    public static void main(String[] args) {
+
+        double priceOfStockPerShare = 21.77;
+        int numberOfSharesBought = 600;
+
+        final double stockBrokerCommissionPercent =0.02; //2%
+
+        double amountPaidForTheStockAlone = numberOfSharesBought * priceOfStockPerShare;
+        double amountOfCommission = amountPaidForTheStockAlone * stockBrokerCommissionPercent;
+        double totalAmountPaid = amountOfCommission + amountPaidForTheStockAlone;
+        System.out.println("The amount paid for the stock alone is: "+amountPaidForTheStockAlone);
+        System.out.println("The amount of commission is: "+ amountOfCommission);
+        System.out.println("The total amount paid is "+ totalAmountPaid);
+    }
+}
