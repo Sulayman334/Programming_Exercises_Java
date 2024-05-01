8065d429a167e0d9cfa880cea16f01e9c6088c45
Sulayman334 <148399915+Sulayman334@users.noreply.github.com>
2024-05-01 22:21:36 +0000

    another commit

---
+++ src/Chapter 2/EnergyDrinkConsumption.java
@@ -0,0 +1,15 @@
+public class EnergyDrinkConsumption {
+    public static void main(String[] args) {
+
+        int numberOfCustomersSurveyed = 12467;
+        double customersPercentageThatPurchasedOneOrMoreDrinks = 0.14; //14%
+        double customersPercentageThatPurchaseCitrus = 0.64;
+
+        int oneOrMoreDrinks = (int) (numberOfCustomersSurveyed * customersPercentageThatPurchasedOneOrMoreDrinks);
+        int citrusPurchase = (int) (oneOrMoreDrinks * customersPercentageThatPurchaseCitrus);
+        System.out.println(oneOrMoreDrinks);
+        System.out.println(citrusPurchase);
+
+
+    }
+}
---
+++ src/Chapter 2/IngredientAdjuster.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class IngredientAdjuster {
+    public static void main(String[] args) {
+
+        double cupOfSugar = 1.5;
+        double cupOfButter = 1;
+        double cupOfFlour = 2.75;
+        int numberOfCookiesProducedWithTheseRecipes= 48;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many cookies do you want to make: ");
+        int numberOfCookies = sc.nextInt();
+
+        double newCupOfButter =  (cupOfButter * numberOfCookies ) / numberOfCookiesProducedWithTheseRecipes;
+        double newCupOfSugar = (cupOfSugar * numberOfCookies ) / numberOfCookiesProducedWithTheseRecipes;
+        double newCupOfFlour = (cupOfFlour * numberOfCookies ) / numberOfCookiesProducedWithTheseRecipes;
+        System.out.println("Cups of butter: "+newCupOfButter);
+        System.out.println("Cups of sugar: "+newCupOfSugar);
+        System.out.println("Cups of flour: " + newCupOfFlour);
+
+
+    }
+}
