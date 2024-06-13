63f4ea6f7ce603ab375447e62c59c498afaa6fdb
Sulayman334 <148399915+Sulayman334@users.noreply.github.com>
2024-06-13 17:51:02 +0000

    first commit

---
+++ src/Chapter_3/TheSpeedOfSound.java
@@ -0,0 +1,32 @@
+package Chapter_3;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class TheSpeedOfSound {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the medium (air, water or steel)");
+        String medium = keyboard.nextLine();
+
+        System.out.println("Enter the distance "+ medium+ " travels");
+        double distance = keyboard.nextInt();
+
+        double amountOfTimeInAir,amountOfTimeInWater, amountOfTimeInSteel;
+
+        if (Objects.equals(medium, "air")) {
+            amountOfTimeInAir = distance /1100;
+            System.out.println(amountOfTimeInAir);
+        } else
+            if (Objects.equals(medium,"water")) {
+            amountOfTimeInWater = distance /4900;
+                System.out.println(amountOfTimeInWater);
+        }else
+            if (Objects.equals(medium,"steel")){
+                amountOfTimeInSteel = distance /16900;
+                System.out.println(amountOfTimeInSteel);
+            }
+    }
+}
