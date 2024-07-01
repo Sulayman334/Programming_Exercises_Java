db9a4edbca7511cd6bc4485a7b27e18c9e686f95
Sulayman334 <sulaymancolley526@gmail.com>
2024-07-01 21:14:10 +0000

    first commit

---
+++ src/Chapter_4/CoinToss.java
@@ -0,0 +1,17 @@
+package Chapter_4;
+
+import java.util.Random;
+
+public class CoinToss {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        for (int count =0; count < 10; count++){
+            if (rand.nextInt(2) == 0){
+                System.out.println("Tails");
+            }else {
+                System.out.println("Heads");
+            }
+        }
+    }
+}
