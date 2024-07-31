6d42f6bd8dc2678773f851b2f13ad49c3b31dd3c
Sulayman334 <sulaymancolley526@gmail.com>
2024-07-31 08:45:08 +0000

    first commit

---
+++ src/Chapter_6/CarClassTest.java
@@ -0,0 +1,24 @@
+package Chapter_6;
+
+public class CarClassTest {
+    public static void main(String[] args) {
+
+        CarClass myCar = new CarClass(2004,"SULLY CARS",250);
+        myCar.accelerate(5);
+        myCar.accelerate(5);
+        myCar.accelerate(5);
+        myCar.accelerate(5);
+        myCar.accelerate(5);
+
+
+        System.out.println(myCar.getSpeed());
+
+        myCar.brake(5);
+        myCar.brake(5);
+        myCar.brake(5);
+        myCar.brake(5);
+        myCar.brake(5);
+
+        System.out.println(myCar.getSpeed());
+    }
+}
