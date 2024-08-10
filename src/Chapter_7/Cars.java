45de144608522fd0e082dfa9f726e14d2cc75f52
Sulayman334 <sulaymancolley526@gmail.com>
2024-08-10 12:57:22 +0000

    Commitment

---
+++ src/Chapter_7/Cars.java
@@ -0,0 +1,12 @@
+package Chapter_7;
+
+public class Cars {
+    public static void main(String[] args) {
+
+        String [] cars = {"Volvo", "Benz","Golf"};
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars [i]);
+        }
+    }
+}
