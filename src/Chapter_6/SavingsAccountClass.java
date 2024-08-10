080551d8105c8396278541c600cd33e1a01b7de0
Sulayman334 <sulaymancolley526@gmail.com>
2024-08-10 12:28:38 +0000

    Commitment

--- src/Chapter_6/SavingsAccountClass.java
+++ src/Chapter_6/SavingsAccountClass.java
@@ -9,4 +9,47 @@ public class SavingsAccountClass {
     private double totalInterestEarned = 0;
 
 
+    public SavingsAccountClass (double StartingBalance){
+        this.StartingBalance = StartingBalance;
+    }
+
+    public void deposit(double amount){
+        StartingBalance += amount;
+        totalDeposits += amount;
+
+    }
+
+    public void withdraw(double amount){
+        StartingBalance -= amount;
+        totalWithdrawals += amount;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate){
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public void addMonthlyInterest() {
+        double monthlyInterestRate = annualInterestRate / 12;
+        double interest = StartingBalance * monthlyInterestRate;
+        StartingBalance += interest;
+        totalInterestEarned += interest;
+    }
+
+    public double getStartingBalance(){
+        return StartingBalance;
+    }
+
+    public double getTotalDeposits(){
+        return totalDeposits;
+    }
+
+    public double getTotalWithdrawals(){
+        return totalWithdrawals;
+    }
+
+    public double getTotalInterestEarned(){
+        return totalInterestEarned;
+    }
+
+
 }
