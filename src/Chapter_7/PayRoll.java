3461f9feaa6d1c9d95c3c82a907dde8457cfcc3e
Sulayman334 <sulaymancolley526@gmail.com>
2024-08-27 00:00:32 +0000

    Commitment

---
+++ src/Chapter_7/PayRoll.java
@@ -0,0 +1,11 @@
+package Chapter_7;
+
+import java.util.Scanner;
+
+public class PayRoll {
+    private double[] sales;
+
+    public PayRoll(double[] sales) {
+        sales = new double[sales.length];
+    }
+}
